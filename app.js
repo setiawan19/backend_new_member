a362b146755a152097a7a7bceb91551d08d1b418
setiawan <setiawansocial@gmail.com>
2019-06-22 02:55:01 +0700

    add API

--- app.js
+++ app.js
@@ -18,10 +18,20 @@ db.connect(() => {
   // db.end()
 });
 // ===================== INPUT API =========================
-//ambil all data admin
+
+//ambil all data customer
 app.get("/", (req, res) => {
-  var ambildata = `SELECT * FROM customer`;
-  db.query(ambildata, (err, result) => {
+  var sql = `SELECT customer.nama AS nama, customer.tempat_lahir AS lahir,
+                    customer.tanggal_lahir AS birth, customer.alamat AS alamat,
+                    customer.status_member AS member, penghasilan.status AS penghasilan,
+                    status_rumah.status AS rumah, status_nikah.status AS nikah,
+                    status_anak.status AS anak
+                    FROM customer join penghasilan, status_anak, status_nikah, status_rumah
+                    WHERE customer.id_kawin = status_nikah.id
+                    AND customer.id_penghasilan = penghasilan.id
+                    AND customer.id_anak = status_anak.id
+                    AND customer.id_rumah = status_rumah.id`;
+  db.query(sql, (err, result) => {
     if (err) {
       throw err;
     } else {
@@ -30,6 +40,93 @@ app.get("/", (req, res) => {
     }
   });
 });
+
+//ambil data by status non member
+app.get("/non_member", (req, res) => {
+  var sql = `SELECT customer.nama AS nama, customer.tempat_lahir AS lahir,
+                    customer.tanggal_lahir AS birth, customer.alamat AS alamat,
+                    customer.status_member AS member, penghasilan.status AS penghasilan,
+                    status_rumah.status AS rumah, status_nikah.status AS nikah,
+                    status_anak.status AS anak
+                    FROM customer join penghasilan, status_anak, status_nikah, status_rumah
+                    WHERE customer.id_kawin = status_nikah.id
+                    AND customer.id_penghasilan = penghasilan.id
+                    AND customer.id_anak = status_anak.id
+                    AND customer.id_rumah = status_rumah.id
+                    AND customer.status_member = 0`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+      //console.log(result);
+    }
+  });
+});
+
+//ambil data by status member
+app.get("/member", (req, res) => {
+  var sql = `SELECT customer.nama AS nama, customer.tempat_lahir AS lahir,
+                    customer.tanggal_lahir AS birth, customer.alamat AS alamat,
+                    customer.status_member AS member, penghasilan.status AS penghasilan,
+                    status_rumah.status AS rumah, status_nikah.status AS nikah,
+                    status_anak.status AS anak
+                    FROM customer join penghasilan, status_anak, status_nikah, status_rumah
+                    WHERE customer.id_kawin = status_nikah.id
+                    AND customer.id_penghasilan = penghasilan.id
+                    AND customer.id_anak = status_anak.id
+                    AND customer.id_rumah = status_rumah.id
+                    AND customer.status_member = 1`;
+  // var sql = `SELECT * FROM customer`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+      //console.log(result);
+    }
+  });
+});
+
+//add data calon member
+app.post("/add", (req, res) => {
+  var data = {
+    id: req.body.id,
+    nama: req.body.nama,
+    id_kawin: req.body.id_kawin,
+    id_penghasilan: req.body.id_penghasilan,
+    id_rumah: req.body.id_rumah,
+    id_anak: req.body.id_anak,
+    tempat_lahir: req.body.tempat_lahir,
+    tanggal_lahir: req.body.tanggal_lahir,
+    alamat: req.body.alamat,
+    status_member: req.body.status_member
+  };
+
+  var sql = `INSERT INTO customer SET ?`;
+
+  db.query(sql, data, (err, result) => {
+    if (err) {
+      console.log("anda tidak dapat insert data member baru");
+      throw err;
+    } else {
+      res.send({
+        status: "Data Member Baru Berhasil di Tambahkan",
+        id: req.body.id,
+        nama: req.body.nama,
+        id_kawin: req.body.id_kawin,
+        id_penghasilan: req.body.id_penghasilan,
+        id_rumah: req.body.id_rumah,
+        id_anak: req.body.id_anak,
+        tempat_lahir: req.body.tempat_lahir,
+        tanggal_lahir: req.body.tanggal_lahir,
+        alamat: req.body.alamat,
+        status_member: req.body.status_member
+      });
+    }
+  });
+});
+
 // aktivasi server
 app.listen(8000, () => {
   console.log("Server aktif di port 8000!");
